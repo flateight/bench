c593ef5d131fe54f312c60a146381b421b574d20
Shinya Matsui <98242377+flateight@users.noreply.github.com>
2024-12-20 16:22:29 +0900

    latency をaverageで表示する

--- cal-json.sh
+++ cal-json.sh
@@ -17,13 +17,18 @@ do
   IOPS2=`cat $LOG | jq ".jobs[$i].write.iops"`
   IOPS=`echo "scale=0; $IOPS1 + $IOPS2"| bc | sed 's/\..*//g'`
 
-  NS=`cat $LOG | jq ".jobs[$i].latency_ns" | sed 's/,$//g' | awk 'BEGIN{n=0;m=0} /^ / {if(m<$2){ m=$2 ;n=$1 }} END{print "ns "n" "m}'`
-  US=`cat $LOG | jq ".jobs[$i].latency_us" | sed 's/,$//g' | awk 'BEGIN{n=0;m=0} /^ / {if(m<$2){ m=$2 ;n=$1 }} END{print "us "n" "m}'`
-  MS=`cat $LOG| jq ".jobs[$i].latency_ms" | sed 's/,$//g' | awk 'BEGIN{n=0;m=0} /^ / {if(m<$2){ m=$2 ;n=$1 }} END{print "ms "n" "m}'`
-  echo $NS > $WORK
-  echo $US >> $WORK
-  echo $MS >> $WORK
-  LATENCY=`cat $WORK | awk 'BEGIN{n=0;m=0;name} {if(m<$3){ m=$3 ;n=$2; name=$1 }} END{print name n m}'`
+  LL=`cat $LOG| jq ".jobs[$i].read.clat_ns.mean"`
+  if [ $(echo "$LL + 0" | bc) == "0" ];then
+	  LL=`cat $LOG| jq ".jobs[$i].write.clat_ns.mean"`
+  fi
+  #NS=`cat $LOG | jq ".jobs[$i].latency_ns" | sed 's/,$//g' | awk 'BEGIN{n=0;m=0} /^ / {if(m<$2){ m=$2 ;n=$1 }} END{print "ns "n" "m}'`
+  #US=`cat $LOG | jq ".jobs[$i].latency_us" | sed 's/,$//g' | awk 'BEGIN{n=0;m=0} /^ / {if(m<$2){ m=$2 ;n=$1 }} END{print "us "n" "m}'`
+  #MS=`cat $LOG| jq ".jobs[$i].latency_ms" | sed 's/,$//g' | awk 'BEGIN{n=0;m=0} /^ / {if(m<$2){ m=$2 ;n=$1 }} END{print "ms "n" "m}'`
+  #echo $NS > $WORK
+  #echo $US >> $WORK
+  #echo $MS >> $WORK
+  #LATENCY=`cat $WORK | awk 'BEGIN{n=0;m=0;name} {if(m<$3){ m=$3 ;n=$2; name=$1 }} END{print name n m}'`
+  LATENCY=$(echo $LL / 1000 | bc)
 
   printf " %20s bw:%5s iops:%5s latency:%5s\n" $NAME $BW $IOPS $LATENCY
 done
